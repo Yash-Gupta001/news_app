188a884b5172728b6def36a0a2f88e9416418b89
Yash-Gupta001 <gyash4963@gmail.com>
2025-04-28 12:45:31 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:news_app/config/theme.dart';
+import 'package:news_app/pages/myhomepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'News App',
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      themeMode: ThemeMode.system,
+      darkTheme: darkTheme,
+      home: const Homepage(),
+    );
+  }
+}
